2b5a23691109cd799e92d5f71abfb4a86ffad7e7
Niko Craft <nermion@gmail.com>
2019-09-08 17:39:36 +0200

    refactor: update command

--- app/Console/Commands/Update.php
+++ app/Console/Commands/Update.php
@@ -41,39 +41,39 @@ class Update extends Releases
     public function handle()
     {
         $websiteService = new WebsiteService;
-        $release = get_website_setting('laraone.phoenix');
-        $lastReleaseVersion = $this->getLastVersion();
+        $phoenixCurrentVersion = get_website_setting('laraone.phoenix');
+        $phoenixLastVersion = $this->getLastVersion();
 
         Artisan::call('config:clear');
         Artisan::call('config:cache');
-        
-        if($this->option('fetch-latest')) {
-            $this->fetchLatestRelease();
-            $zip = new ZipArchive;
 
-            if ($zip->open(storage_path('releases'. DIRECTORY_SEPARATOR . $lastReleaseVersion . '.zip')) === TRUE) {
-                $base = 'phoenix-' . $lastReleaseVersion . DIRECTORY_SEPARATOR;
-                $this->info('Unpacking latest release.');
-                $zip->extractSubdirTo($base .'app', base_path('app'));
-                $zip->extractSubdirTo($base .'database', base_path('database'));
-                $zip->extractSubdirTo($base .'resources', base_path('resources'));
-                $zip->extractSubdirTo($base .'routes', base_path('routes'));
-                $zip->extractSubdirTo($base .'config', base_path('config'));
-                $zip->extractSubdirTo($base .'public' . DIRECTORY_SEPARATOR . 'install', base_path('public' . DIRECTORY_SEPARATOR . 'install'));
-                exec('composer dump-autoload');
-                $this->info('Successfully unpacked the release.');
-            } else {
-                $this->info('Not able to unpack zip and proceed with the update. Please report this problem.');
-            }
-        }
-
-        $currentReleaseIndex = $this->getReleaseIndex($release);
+        $phoenixCurrentReleaseIndex = $this->getReleaseIndex($phoenixCurrentVersion);
         $lastReleaseIndex = $this->getLastIndex();
 
-        if($currentReleaseIndex != $lastReleaseIndex) {
-            $this->info('Update started, current version is ' . $release);
+        if($phoenixCurrentReleaseIndex != $lastReleaseIndex) {
+            $this->info('Update started, current version is ' . $phoenixCurrentVersion);
 
-            $updatesData = array_slice($this->releasesData, $currentReleaseIndex);
+            if($this->option('fetch-latest')) {
+                $this->fetchLatestRelease();
+                $zip = new ZipArchive;
+    
+                if ($zip->open(storage_path('releases'. DIRECTORY_SEPARATOR . $phoenixLastVersion . '.zip')) === TRUE) {
+                    $base = 'phoenix-' . $phoenixLastVersion . DIRECTORY_SEPARATOR;
+                    $this->info('Unpacking latest release.');
+                    $zip->extractSubdirTo($base .'app', base_path('app'));
+                    $zip->extractSubdirTo($base .'database', base_path('database'));
+                    $zip->extractSubdirTo($base .'resources', base_path('resources'));
+                    $zip->extractSubdirTo($base .'routes', base_path('routes'));
+                    $zip->extractSubdirTo($base .'config', base_path('config'));
+                    $zip->extractSubdirTo($base .'public' . DIRECTORY_SEPARATOR . 'install', base_path('public' . DIRECTORY_SEPARATOR . 'install'));
+                    exec('composer dump-autoload');
+                    $this->info('Successfully unpacked the release.');
+                } else {
+                    $this->info('Not able to unpack zip and proceed with the update. Please report this problem.');
+                }
+            }
+
+            $updatesData = array_slice($this->releasesData, $phoenixCurrentReleaseIndex);
             $seeded = 0;
 
             foreach($updatesData as $key => $value) {
@@ -99,8 +99,8 @@ class Update extends Releases
             // exec('composer dump-autoload');
 
             $this->info('About to download latest admin and default theme.');
-            $this->fetchAdminTheme($lastReleaseVersion);
-            $this->fetchDefaultTheme($lastReleaseVersion);
+            $this->fetchAdminTheme($phoenixLastVersion);
+            // $this->fetchDefaultTheme($phoenixLastVersion);
             $this->info('Themes latest versions downloaded.');
 
             $websiteService->updateSetting('laraone', 'phoenix', $this->getLastVersion());
@@ -112,19 +112,11 @@ class Update extends Releases
 
     protected function fetchLatestRelease()
     {
+        Artisan::call('config:clear');
+        Artisan::call('config:cache');
         $this->fetchLatestReleaseData();
-        
-        $currentVersion = get_website_setting('laraone.phoenix');
-        $lastReleaseVersion = $this->getLastVersion();
-
-        if($currentVersion != $lastReleaseVersion) {
-            $this->fetchRelease($lastReleaseVersion);
-            $this->fetchDefaultTheme($lastReleaseVersion);
-        } else {
-            $this->info('Already up to latest release.');
-        }
-
-        return $lastReleaseVersion;
+        $phoenixLastVersion = $this->getLastVersion();
+        $this->fetchRelease($phoenixLastVersion);
     }
 
     protected function getSeedFileName($version)
