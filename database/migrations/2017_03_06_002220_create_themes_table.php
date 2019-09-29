08a1178dd1e55e7fadb5cc1896e4b9f7ca5e7aec
Niko Craft <nermion@gmail.com>
2019-09-29 21:13:08 +0200

    fix: set unique columns

--- database/migrations/2017_03_06_002220_create_themes_table.php
+++ database/migrations/2017_03_06_002220_create_themes_table.php
@@ -15,16 +15,16 @@ class CreateThemesTable extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('namespace');
-            $table->string('name');
-            $table->string('folder')->nullable();
+            $table->string('namespace')->unique();
+            $table->string('name')->unique();
+            $table->string('folder')->unique();
             $table->string('org');
             $table->string('author');
+            $table->string('url');
             $table->string('version');
             $table->text('description');
             $table->tinyInteger('screenshots')->default(1);
             $table->integer('parent')->nullable();
-            $table->string('url');
             $table->string('releases_url');
             $table->string('download_url');
             $table->timestamps();
