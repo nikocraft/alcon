644e0376b4358c053f7efffe05a5c150f86b8dd2
Niko Craft <nermion@gmail.com>
2019-12-21 11:59:01 +0100

    refactor: update migration name

--- database/migrations/2017_04_09_194412_create_settings_table.php
+++ database/migrations/2017_04_09_194412_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateWebsiteSettingsTable extends Migration
+class CreateSettingsTable extends Migration
 {
     /**
      * Run the migrations.
