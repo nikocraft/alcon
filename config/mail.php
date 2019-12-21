3b3d48160a16bfa7b84c8d1cf4ec7e7fb9c0b338
Niko Craft <nermion@gmail.com>
2019-12-21 04:17:46 +0100

    add new mail channel to config/mail

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
     |
     */
 
-    'sendmail' => '/usr/sbin/sendmail -bs',
+    'sendmail' => env('MAIL_SENDMAIL', '/usr/sbin/sendmail -bs'),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,5 +119,17 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | If you are using the "log" driver, you may specify the logging channel
+    | if you prefer to keep mail messages separate from other log entries
+    | for simpler reading. Otherwise, the default channel will be used.
+    |
+    */
+    'log_channel' => env('MAIL_LOG_CHANNEL'),
 
 ];
